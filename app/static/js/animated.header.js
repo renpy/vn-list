184ad86189384691e0bae0c51c29429559c609de
lzavsek <leon.zavsek@gmail.com>
2014-09-21 18:30:15 +0200

    Sidebar hiding on small window sizes

--- app/static/js/animated.header.js
+++ app/static/js/animated.header.js
@@ -95,7 +95,7 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-fixed-top' ),
 		didScroll = false,
-		changeHeaderOn = 300;
+		changeHeaderOn = 100;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
