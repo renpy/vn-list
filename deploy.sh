261afa84630f18c9f9082db836d318a511398d82
Tom Rothamel <tom@rothamel.us>
2014-10-12 22:56:07 -0400

    Remove social sharing widget.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 ROOT=$(dirname $(readlink -f $0))
 cd $ROOT
 
-rsync -av . leon@sachiko.onegeek.org:/home/leon/wsgi.renaius --exclude .git
+rsync -av . leon@sachiko.onegeek.org:/home/leon/wsgi.renaius --exclude .git --exclude tmp
 ssh leon@sachiko.onegeek.org touch /home/leon/wsgi.renaius/main.wsgi
